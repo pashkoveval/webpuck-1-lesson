d595608e4ffdf4ce098a38635bc5d1187932264a
EVGENIY PASHKOV <61497581+pashkoveval@users.noreply.github.com>
2021-03-20 09:39:17 +0300

    Вроде все переделал как вы подсказывали=)

---
+++ js/Dependencies/datecalc.js
@@ -0,0 +1,27 @@
+import {errorFunction} from './errorMSG.js';
+import {diffDates,diffToHTML} from './diffDate.js';
+
+const datecalcForm = document.getElementById('datecalc');
+const datecalcFresalt = document.getElementById('datecalc__res');
+datecalcForm.addEventListener('submit', handlCalcDates);
+
+function handlCalcDates(event) {
+	event.preventDefault();
+	datecalcFresalt.innerHTML = '';
+	let {
+		firstDate,
+		secondDate
+	} = event.target.elements;
+	firstDate = firstDate.value;
+	secondDate = secondDate.value;
+
+	if (firstDate && secondDate) {
+		const result = diffDates(firstDate, secondDate);
+		datecalcFresalt.innerHTML = diffToHTML(result);
+	} else {
+		datecalcFresalt.innerHTML = errorFunction('Все поля должны бить заполнены!');
+	}
+
+
+
+}
